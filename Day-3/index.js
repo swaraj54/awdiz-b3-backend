7bae5b961c11516ec9164988d006f550efb10339
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2026-02-28 14:08:10 +0530

    Upddates for day 3

---
+++ Day-3/index.js
@@ -0,0 +1,63 @@
+import express from "express";
+
+const app = express();
+
+// use - middleware to parse JSON bodies
+app.use(express.json()); // JSON -> JS
+
+const usersList = [
+  { id: 1, name: "Alice" },
+  { id: 2, name: "Bob" },
+  { id: 3, name: "Charlie" },
+];
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.get("/get-users", (req, res) => {
+  res.json({ users: usersList });
+});
+
+app.post("/add-user", (req, res) => {
+  console.log(req.body);
+  usersList.push(req.body);
+  res.json({ message: "User added successfully", users: usersList });
+});
+
+// ROute paramter
+// query parameter
+app.put("/update-user/:id", (req, res) => {
+  console.log(req.params.id);
+  const userId = parseInt(req.params.id);
+
+  const userIndex = usersList.findIndex((user) => user.id === userId);
+
+  console.log(userIndex, "userIndex 2");
+
+  if (userIndex === -1) {
+    return res.status(404).json({ message: "User not found" });
+  } else {
+    usersList[userIndex] = { id: userId, name: req.body.name };
+  }
+  res.json({
+    users: usersList,
+    message: `User with id ${req.params.id} updated successfully`,
+  });
+});
+
+app.delete("/delete-user", (req, res) => {
+  console.log(req.query.user);
+  const userId = parseInt(req.query.user);
+
+  const userIndex = usersList.findIndex((user) => user.id === userId);
+  if (userIndex === -1) {
+    return res.status(404).json({ message: "User not found" });
+  } else {
+    usersList.splice(userIndex, 1);
+  }
+  res.json({ message: "User deleted successfully", users: usersList });
+});
+
+app.listen(8000, () => {
+  console.log("Server is running on port 8000");
+});
