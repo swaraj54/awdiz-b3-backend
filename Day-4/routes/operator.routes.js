bf34fddd3d7fc2abb42e7ecfcd7551e3e4b264c9
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2026-03-21 13:05:59 +0530

    Updates

--- Day-4/routes/operator.routes.js
+++ Day-4/routes/operator.routes.js
@@ -5,17 +5,17 @@ const OperatorRouter = Router();
 
 OperatorRouter.get("/", async (req, res) => {
   try {
-    const products = await ProductSchema.find({ price: { $eq: 119999 } });
-    const products = await ProductSchema.find({ price: { $ne: 119999 } });
-    const productsIN = await ProductSchema.find({ price: { $in: [119999, 79000] } });
-    const productsNIN = await ProductSchema.find({ price: { $nin: [119999, 79000] } });
+    // const products = await ProductSchema.find({ price: { $eq: 119999 } });
+    // const products = await ProductSchema.find({ price: { $ne: 119999 } });
+    // const productsIN = await ProductSchema.find({ price: { $in: [119999, 79000] } });
+    // const productsNIN = await ProductSchema.find({ price: { $nin: [119999, 79000] } });
 
-    const productsAND = await ProductSchema.find({
-      $and: [{ price: 119999 }, { stock: 8 }],
-    });
-    const productsOR = await ProductSchema.find({
-      $or: [{ price: 119999 }, { stock: 10 }],
-    });
+    // const productsAND = await ProductSchema.find({
+    //   $and: [{ price: 119999 }, { stock: 8 }],
+    // });
+    // const productsOR = await ProductSchema.find({
+    //   $or: [{ price: 119999 }, { stock: 10 }],
+    // });
     const productsNOT = await ProductSchema.find({
       price: { $not: { $gt: 38999 } },
     });
@@ -29,4 +29,66 @@ OperatorRouter.get("/", async (req, res) => {
   }
 });
 
+OperatorRouter.get("/matching-grouping", async (req, res) => {
+  try {
+    const products = await ProductSchema.aggregate([
+      {
+        $match: {
+          category: "Electronics",
+          stock: { $gt: 15 },
+          price: { $gt: 2000 },
+        },
+      },
+      {
+        $group: {
+          _id: "$category",
+          totalStock: { $sum: "$stock" },
+          totalPrice: { $sum: { $multiply: ["$stock", "$price"] } },
+        },
+      },
+    ]);
+    res.send(products);
+  } catch (error) {
+    return res.status(500).json({ error: error });
+  }
+});
+
+OperatorRouter.get("/matching-unwind", async (req, res) => {
+  try {
+    const products = await ProductSchema.aggregate([
+      {
+        $match: {
+          tags: { $exists: true },
+        },
+      },
+      { $unwind: "$tags" },
+      {
+        $group: {
+          _id: "$tags",
+          totalStock: { $sum: "$stock" },
+          totalPrice: { $sum: { $multiply: ["$stock", "$price"] } },
+        },
+      },
+    ]);
+    res.send(products);
+  } catch (error) {
+    return res.status(500).json({ error: error });
+  }
+});
+
+OperatorRouter.get("/matching-projecting", async (req, res) => {
+  try {
+    const products = await ProductSchema.aggregate([
+      {
+        $match: {
+          tags: { $exists: true },
+        },
+      },
+      { $project: { name: 1, price: 1, _id: 0 } },
+    ]);
+    res.send(products);
+  } catch (error) {
+    return res.status(500).json({ error: error });
+  }
+});
 export default OperatorRouter;
